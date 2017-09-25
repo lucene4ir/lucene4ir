e7e97fcedb0dbafa49d23f62b6614076c7099488
Harry Scells <harryscells@gmail.com>
2017-09-25 15:03:04 +0100

    Clean up language model.

--- src/main/java/lucene4ir/utils/LanguageModel.java
+++ src/main/java/lucene4ir/utils/LanguageModel.java
@@ -18,13 +18,13 @@ import java.util.Map;
 public class LanguageModel {
 
     protected IndexReader reader;
-    IndexSearcher searcher;
-    CollectionStatistics collectionStats;
+    private IndexSearcher searcher;
+    private CollectionStatistics collectionStats;
     public String field = Lucene4IRConstants.FIELD_ALL;
-    public int[] doc_ids;
-    public double doc_len;
-    public HashMap<String, Double> termcounts = new HashMap<>();
-    public long token_count;
+    private int[] doc_ids;
+    private double doc_len;
+    private HashMap<String, Double> termCounts = new HashMap<>();
+    private long token_count;
 
     public LanguageModel(IndexReader ir, int doc_id) {
         reader = ir;
@@ -46,11 +46,10 @@ public class LanguageModel {
         reader = ir;
         searcher = new IndexSearcher(reader);
         this.doc_ids = doc_ids;
-        int size = doc_ids.length;
         doc_len = 0.0;
-        for (int i = 0; i < size; i++) {
-            System.out.println("doc id: " +  doc_ids[i]);
-            updateTermCountMap(doc_ids[i], 1.0);
+        for (int doc_id : doc_ids) {
+            System.out.println("doc id: " + doc_id);
+            updateTermCountMap(doc_id, 1.0);
         }
 
         try {
@@ -84,11 +83,9 @@ public class LanguageModel {
         }
     }
 
-
-    public double getDocumentTermProb(String termText) {
-        if (termcounts.containsKey(termText)) {
-            double tf = termcounts.get(termText);
-
+    private double getDocumentTermProb(String termText) {
+        if (termCounts.containsKey(termText)) {
+            double tf = termCounts.get(termText);
             return (tf + 0.0) / (doc_len + 0.0);
         } else {
             System.out.println("Term does not occur in document.");
@@ -96,20 +93,15 @@ public class LanguageModel {
         }
     }
 
-    ;
-
-    public double getDocumentTermCount(String termText) {
-        if (termcounts.containsKey(termText)) {
-            double tf = termcounts.get(termText);
+    private double getDocumentTermCount(String termText) {
+        if (termCounts.containsKey(termText)) {
+            double tf = termCounts.get(termText);
             return (tf + 0.0);
         } else {
             System.out.println("Term does not occur in document.");
             return 0.0;
         }
     }
-
-    ;
-
 
     public double getCollectionTermProb(String termText) {
         double prob = 0.0;
@@ -126,20 +118,20 @@ public class LanguageModel {
     }
 
 
-    protected void updateTermCountMap(int doc_id, double weight) {
+    private void updateTermCountMap(int doc_id, double weight) {
         try {
             Terms t = reader.getTermVector(doc_id, field);
             if ((t != null) && (t.size() > 0)) {
                 TermsEnum te = t.iterator();
-                BytesRef term = null;
+                BytesRef term;
                 PostingsEnum p = null;
                 while ((term = te.next()) != null) {
                     String termText = term.utf8ToString();
-                    if (termcounts.containsKey(termText)) {
-                        double v = termcounts.get(termText);
-                        termcounts.put(termText, v + (te.totalTermFreq() * weight));
+                    if (termCounts.containsKey(termText)) {
+                        double v = termCounts.get(termText);
+                        termCounts.put(termText, v + (te.totalTermFreq() * weight));
                     } else {
-                        termcounts.put(termText, (te.totalTermFreq() * weight));
+                        termCounts.put(termText, (te.totalTermFreq() * weight));
                     }
                     doc_len = doc_len +  (te.totalTermFreq() * weight);
 
@@ -158,30 +150,31 @@ public class LanguageModel {
         return (lambda * getDocumentTermProb(termText)) + (1 - lambda) * getCollectionTermProb(termText);
     }
 
+    @SuppressWarnings("WeakerAccess")
     public double getDirichletTermProb(String termText, double mu) {
         return (getDocumentTermCount(termText) + mu * getCollectionTermProb(termText)) / (doc_len + mu);
     }
 
 
     public void printTermVector() {
-        double tprob = 0.0;
-        double tcount = 0.0;
+        double tProb = 0.0;
+        double tCount = 0.0;
 
-        for (Map.Entry m : termcounts.entrySet()) {
+        for (Map.Entry m : termCounts.entrySet()) {
             String termText = (String) m.getKey();
             double count = getDocumentTermCount(termText);
-            tcount = tcount + count;
+            tCount = tCount + count;
 
             double prob = getDocumentTermProb(termText);
-            double cprob = getCollectionTermProb(termText);
-            double jmprob = getJMTermProb(termText, 0.5);
-            double dirprob = getDirichletTermProb(termText, 100);
-            System.out.println(m.getKey() + " " + m.getValue() + " " + prob + " " + cprob + " " + jmprob + " " + dirprob);
-            tprob = tprob + prob;
+            double cProb = getCollectionTermProb(termText);
+            double jmProb = getJMTermProb(termText, 0.5);
+            double dirProb = getDirichletTermProb(termText, 100);
+            System.out.println(m.getKey() + " " + m.getValue() + " " + prob + " " + cProb + " " + jmProb + " " + dirProb);
+            tProb = tProb + prob;
 
         }
-        System.out.println("Total prob mass: " + tprob + " total term count:" + tcount + " Doc size:" + doc_len);
 
+        System.out.println("Total prob mass: " + tProb + " total term count:" + tCount + " Doc size:" + doc_len);
     }
 
 
