b7419338847addc840ac2fbc6d5730fd1888ad3e
ABDULAZIZALQATAN <47518534+ABDULAZIZALQATAN@users.noreply.github.com>
2019-06-19 05:38:04 +0100

    Add files via upload

---
+++ src/main/java/lucene4ir/AbdulazizClasses/Run Experiments/Output/RetrievalScripts/WAPO-kStem-BM25.sh
@@ -0,0 +1,7 @@
+cd 'C:/Users/kkb19103/Desktop/LUCENE/Run Experiments'
+java retrievalApp output/Params/WAPO-kStem-BM25/WAPO-kStem-BM25-0.0-0.0.params 
+java retrievalApp output/Params/WAPO-kStem-BM25/WAPO-kStem-BM25-0.2-0.2.params 
+java retrievalApp output/Params/WAPO-kStem-BM25/WAPO-kStem-BM25-0.4-0.4.params 
+java retrievalApp output/Params/WAPO-kStem-BM25/WAPO-kStem-BM25-0.6-0.6.params 
+java retrievalApp output/Params/WAPO-kStem-BM25/WAPO-kStem-BM25-0.8-0.8.params 
+java retrievalApp output/Params/WAPO-kStem-BM25/WAPO-kStem-BM25-1.0-1.0.params 
