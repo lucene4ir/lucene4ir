beb1b773bf482971ca623db573fc430633d7d464
ABDULAZIZALQATAN <47518534+ABDULAZIZALQATAN@users.noreply.github.com>
2019-06-24 10:55:52 +0100

    Add files via upload

--- src/main/java/lucene4ir/RetrievabilityCalculator/RetrievabilityCalculator.java
+++ src/main/java/lucene4ir/RetrievabilityCalculator/RetrievabilityCalculator.java
@@ -1,11 +1,14 @@
 package lucene4ir.RetrievabilityCalculator;
 
 import lucene4ir.QueryGenerator.DumpTermsApp;
-import lucene4ir.QueryGenerator.QueryGenerator;
 import lucene4ir.RetrievalApp;
 
+import javax.xml.bind.JAXB;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -18,9 +21,12 @@ public class RetrievabilityCalculator {
 
 
     // Properties
-    private String sourceParameterFile, indexName;
+    public RetrievabilityCalculatorParams p;
+
+
+    private String sourceParameterFile;
     private final String docIDField = "docnum";
-    private HashMap<Integer, Integer> docMap;
+    private HashMap<Integer, Double> docMap , qryMap;
     private double totalWeights;
 
     // Constructor Method
@@ -29,119 +35,162 @@ public class RetrievabilityCalculator {
             sourceParameterFile = "params/RetrievabilityCalculator.xml";
         else
             sourceParameterFile = inputParameters;
-        docMap = new HashMap<Integer, Integer>();
+        docMap = new HashMap<Integer, Double>();
+        qryMap = new HashMap<Integer, Double>();
+    }
+    private void displayMsg(String msg)
+    {
+        System.out.println(msg);
+        System.exit(0);
+    }
+    private void readParamsFromFile() {
+        System.out.println("Reading Param File");
+        try {
+            p = JAXB.unmarshal(new File(sourceParameterFile), RetrievabilityCalculator.RetrievabilityCalculatorParams.class );
+            if (p.indexName.toString().isEmpty())
+                displayMsg ("IndexName Parameter is Missing");
+            System.out.println("Index: " + p.indexName);
+            if (p.outputPath.toString().isEmpty())
+                displayMsg ("Query Output Path Parameter is Missing");
+            if (p.resultFile.toString().isEmpty())
+                displayMsg ("Result File Parameter is Missing");
+            if (p.queryFile.toString().isEmpty())
+                displayMsg ("Query File Parameter is Missing");
+            if (p.maxResults < 1)
+                displayMsg ("Max Results Parameter is Missing");
+
+        } catch (Exception e) {
+            System.out.println(" caught a " + e.getClass() +
+                    "\n with message: " + e.getMessage());
+            System.exit(1);
+        }
     }
 
-    private void displayResults(String outPath, double G) {
+    private void displayResults(String outPath) throws Exception
+    {
 
         /*
-        Display The Results as Needed
+        Display The Results as Needed For the whole Document Vector
          */
         String line;
         Map.Entry item;
         Iterator it = docMap.entrySet().iterator();
-        int ctr;
+        int docID , N , i ;
+        double r , numerator = 0 , denominator = 0 , G;
+
+        N = docMap.size() - 1;
+        i = 1;
+        PrintWriter pr = new PrintWriter(p.outputPath + "/RetrievabilityList.txt");
         while (it.hasNext()) {
             item = (Map.Entry) it.next();
-            ctr = (int) item.getValue();
-            line = String.format("%d %d %f", (int) item.getKey(), ctr, ctr * totalWeights);
-            System.out.println(line);
-        }
-        line = "TotalWeights = " + totalWeights + "\nThe G Coefficient = " + G;
-        System.out.println(line);
-    }
-
-    private double calculateG() {
-
-        /*
-        Calculate Retrievability (R) for each document
-        Calculate G Coefficient from calculated R
-         */
-        double result, numerator = 0, denominator = 0, r;
-        Map.Entry item;
-        int N, i = 0;
-
-        N = docMap.size();
-        Iterator it = docMap.entrySet().iterator();
-        while (it.hasNext()) {
-            item = (Map.Entry) it.next();
-            r = (int) item.getValue() * totalWeights;
-            numerator += (2 * (int) item.getKey() - N - 1) * r;
+            r = (double) item.getValue();
+            docID = (int) item.getKey();
+            // N already deducted  by 1 to save process
+            numerator += (2 * i++ - N) * r;
             denominator += r;
+            line = String.format("%d %f\n", docID, r);
+            pr.print(line);
+            System.out.print(line);
         }
-        result = numerator / (N * denominator);
+
+        // Get N Value Back
+        G = numerator / (++N * denominator);
+        line = "The G Coefficient = " + G;
+        System.out.println(line);
+        pr.print(line);
+        pr.close();
+    }
+
+    private double costFunction (char costFunction , int rank )
+    {
+        double result = 0;
+        if (costFunction == 'c')
+            result = 1;
+        else
+            result = 1.0 / Math.pow(rank,p.maxResults);
+
+        return result;
+    }
+    private double calculateR (int qryID , int rank)
+    {
+        char costFunction;
+        double result = 0 , weight;
+        weight = qryMap.get(qryID);
+        costFunction =  p.costFunction.toLowerCase().charAt(0);
+        result = weight * costFunction(costFunction,rank);
         return result;
     }
 
-    private double getTotalWeights(String inFile) throws Exception {
-        /*
-        Given Query File From QueryGenerator (inFile)
-        Read All Grams Weights and return their total
-         */
-        double result = 0, currentWeight;
-        String line, parts[];
 
-        BufferedReader br = new BufferedReader(new FileReader(inFile));
-        line = br.readLine();
-        while (line != null) {
-            parts = line.split(" ", 7);
-            currentWeight = Double.parseDouble(parts[6]);
-            result += currentWeight;
-            line = br.readLine();
-        }
-        br.close();
-        return result;
-    }
-
-    private void fillDocumentMap(String retrievalResultFile) throws Exception {
-
+    private void readRetrievalResults(String retrievalResultFile) throws Exception {
         /*
         Read RetrievalAPP Results File
         Update the counter ( number of occurances in Results File ) for each document in the Document Map
          */
         String line, parts[];
-        int docid;
+        int docid , qryid , rank ;
+        double r ;
 
         BufferedReader br = new BufferedReader(new FileReader(retrievalResultFile));
+
         line = br.readLine();
         while (line != null) {
-            parts = line.split(" ", 4);
+            parts = line.split(" ", 5);
+            qryid = Integer.parseInt(parts[0]);
             docid = Integer.parseInt(parts[2]);
-            docMap.put(docid, docMap.get(docid) + 1);
+            rank = Integer.parseInt(parts[3]);
+            r = calculateR(qryid,rank);
+            docMap.put(docid, docMap.get(docid) + r);
             line = br.readLine();
         }
         br.close();
     }
 
+    private void initQueryMap(String qryFile ) throws Exception
+    {
+        String line, parts[];
+        int qryID;
+        double weight;
+
+        BufferedReader br = new BufferedReader(new FileReader(qryFile));
+        line = br.readLine();
+        while (line != null)
+        {
+            parts = line.split(" ",7);
+            qryID = Integer.parseInt(parts[0]);
+            weight = Double.parseDouble(parts[6]);
+            qryMap.put(qryID,weight);
+            line = br.readLine();
+        } // End While
+        br.close();
+    }
     private void initDocumentMap() throws Exception {
         // Initialize Document Hash MAP (docid , docCounter = 0)
         DumpTermsApp dump = new DumpTermsApp();
-        dump.indexName = indexName;
+        dump.indexName = p.indexName;
         dump.openReader();
         int docid = 0;
         for (int i = 0; i < dump.reader.maxDoc(); i++) {
             docid = Integer.parseInt(dump.reader.document(i).get(docIDField));
-            docMap.put(docid, 0);
+            docMap.put(docid,0.0);
         }
     }
 
     public void main() {
 
         // Mystro Method that coordinate the process
-        double G;
         try {
-            QueryGenerator qg = new QueryGenerator(sourceParameterFile);
-            qg.generateQueries();
-            indexName = qg.p.indexName;
+            readParamsFromFile();
             RetrievalApp re = new RetrievalApp(sourceParameterFile);
             re.processQueryFile();
             initDocumentMap();
-            fillDocumentMap(qg.p.resultFile);
-            totalWeights = getTotalWeights(qg.p.queryFile.replace("short", "long"));
-            G = calculateG();
-            displayResults(qg.p.gramsOutputPath + "/RetrievabilityScores.txt", G);
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            initQueryMap(p.queryFile.replace("short","long"));
+            readRetrievalResults(p.resultFile);
+            displayResults(p.outputPath + "/RetrievabilityScores.txt");
+        } catch (Exception e) {
+            System.out.println(" caught a " + e.getClass() +
+                    "\n with message: " + e.getMessage());
+            System.exit(1);
         }
     }
 
@@ -150,4 +199,12 @@ public class RetrievabilityCalculator {
         RetrievabilityCalculator rc = new RetrievabilityCalculator("");
         rc.main();
     }
+
+    @XmlRootElement(name = "RetrievabilityCalculatorParams")
+    static
+    public class RetrievabilityCalculatorParams {
+        public String indexName , outputPath,resultFile , queryFile , costFunction;
+        public int maxResults;
+    }
 }
+
