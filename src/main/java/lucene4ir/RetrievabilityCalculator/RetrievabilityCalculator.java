f412473618fcc58199d3bb55f753a9dded7e9594
ABDULAZIZALQATAN <47518534+ABDULAZIZALQATAN@users.noreply.github.com>
2019-06-25 15:08:12 +0100

    Add files via upload

--- src/main/java/lucene4ir/RetrievabilityCalculator/RetrievabilityCalculator.java
+++ src/main/java/lucene4ir/RetrievabilityCalculator/RetrievabilityCalculator.java
@@ -9,9 +9,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 
 // Created by Abdulaziz on 19/06/2019.
@@ -67,35 +65,52 @@ public class RetrievabilityCalculator {
         }
     }
 
+    private double calculateG(ArrayList<Double> sortedRValues)
+    {
+        /*
+        Given sorted array of R values >>> Calculate G Coefficient
+         */
+        int N;
+        double r , numerator = 0 , denominator = 0 , result = 0;
+
+        N = sortedRValues.size() - 1;
+        for (int i = 1 ; i <= sortedRValues.size() ; i++)
+        {
+            r = sortedRValues.get(i-1);
+            numerator += (2 * i - N) * r;
+            denominator += r;
+        }
+        result = numerator / (++N * denominator);
+        return result;
+    }
+
     private void displayResults(String outPath) throws Exception
     {
-
         /*
         Display The Results as Needed For the whole Document Vector
          */
         String line;
         Map.Entry item;
         Iterator it = docMap.entrySet().iterator();
-        int docID , N , i ;
-        double r , numerator = 0 , denominator = 0 , G;
+        int docID ;
+        double r , G;
+        ArrayList<Double> rValues = new ArrayList<Double>();
 
-        N = docMap.size() - 1;
-        i = 1;
         PrintWriter pr = new PrintWriter(p.retFile);
         while (it.hasNext()) {
             item = (Map.Entry) it.next();
             r = (double) item.getValue();
             docID = (int) item.getKey();
             // N already deducted  by 1 to save process
-            numerator += (2 * i++ - N) * r;
-            denominator += r;
+            rValues.add(r);
             line = String.format("%d %f\n", docID, r);
             pr.print(line);
             System.out.print(line);
         }
 
-        // Get N Value Back
-        G = numerator / (++N * denominator);
+        Collections.sort(rValues);
+        G = calculateG(rValues);
+        
         line = "The G Coefficient = " + G;
         System.out.println(line);
         pr.close();
@@ -111,7 +126,7 @@ public class RetrievabilityCalculator {
     private double calculateR (int qryID , int rank)
     {
         double result  , weight;
-        weight = qryMap.get(qryID);
+       weight = qryMap.get(qryID);
         result = weight * costFunction(rank);
         return result;
     }
@@ -135,6 +150,7 @@ public class RetrievabilityCalculator {
             rank = Integer.parseInt(parts[3]);
             r = calculateR(qryid,rank);
             docMap.put(docid, docMap.get(docid) + r);
+           /* docMap.put(docid, docMap.get(docid) + 1);*/
             line = br.readLine();
         }
         br.close();
